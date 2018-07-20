3a5d7873fc825d2bf815fa7ba8e6c95f0c8ea5c7
Josiah Maina <josiah5115@gmail.com>
2018-07-20 12:22:16 -0600

    wizard input fields

--- src/component/Wizard/Wizard.js
+++ src/component/Wizard/Wizard.js
@@ -23,6 +23,18 @@ class Wizard extends Component {
               <button>Cancel</button>
             </Link>
           </div>
+        <div className="inputBox">
+          <br/>
+          <input type='text' placeholder='Property Name' onChange={this.handlePropertyNameChange}/>
+          <br/>
+          <br/>
+          <input type='text' placeholder='Address' onChange={this.handleAddressChange}/>
+          <br/>
+          <br/>
+          <input type='text' placeholder='City' onChange={this.handleCityChange}/>
+          <input type='text' placeholder='State' onChange={this.handleStateChange}/>
+          <input type='number' placeholder='Zip' onChange={this.handleZipChange}/>
+        </div>    
       </div>
     );
   }
