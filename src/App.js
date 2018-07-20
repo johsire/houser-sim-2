14597525a9ac45cea03149c65af266d1e4dd85e3
Josiah Maina <josiah5115@gmail.com>
2018-07-20 11:37:32 -0600

    routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-import Dashboard from './component/Dashboard/Dashboard';
 import Header from './component/Header/Header';
-import Wizard from './component/Wizard/Wizard';
+import route from './route';
 import './App.css';
-
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Dashboard />
-        <Header />
-        <Wizard />
+        <nav className='nav'>
+          <Header />
+          <Link to="/" className='links'>Dashboard</Link>
+          <Link to="/Wizard" className='links'>Wizard</Link> 
+        </nav>
+        {route}
       </div>
-    );
-  }
+    )}
 };
 
 export default App;
