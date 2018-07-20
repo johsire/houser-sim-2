86e15f9890187f6318091b61b98006c63ac7712f
Josiah Maina <josiah5115@gmail.com>
2018-07-20 10:57:48 -0600

    nodemon running, connected to db

--- server/server.js
+++ server/server.js
@@ -1,21 +1,21 @@
 require('dotenv').config();
 const massive = require('massive');
-const espress = require('express');
+const express = require('express');
 const bodyParser = require('body-parser');
 
-import controller from './controller'
+const controller = require ('./controller');
 
 const app = express();
 
 massive(process.env.CONNECTION_STRING)
-.then(dbInstance => {
-  console.log(dbInstance)
-  app.set('db', dbInstance)})
-.catch(err = console.log(err, 'Alert! Error! We got one!'))
+  .then(dbInstance => {
+   app.set('db', dbInstance)})
+  .catch(err => console.log(err));
+
 app.use(bodyParser.json());
 
 
 const SERVER_PORT = process.env.SERVER_PORT || 4200;
 app.listen(SERVER_PORT, () => {
-  console.log(`Kevin Hart cracking em up on port: ${SERVER_PORT}!!`);
+  console.log(`Kevin Hart cracking em up on port: ~ ${SERVER_PORT}!`);
 });
